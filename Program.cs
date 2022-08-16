67a2f797caf4cfe95ab1e57669ff61b7cf935ab6
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2022-08-16 21:58:10 +0200

    simplify

--- Program.cs
+++ Program.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.Resource;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-builder.Services.AddAuthorization(options => 
-    options.AddPolicy("Policy_Role_A", authBuilder => authBuilder.RequireRole("app-role-A")));
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services.AddAuthorization(options => options.AddPolicy("Policy_Role_A", authBuilder => authBuilder.RequireRole("app-role-A")));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,8 +17,8 @@ builder.Services.AddSwaggerGen(options => {
         {
             AuthorizationCode = new OpenApiOAuthFlow
             {
-                AuthorizationUrl = new Uri("https://login.microsoftonline.com/b3edbf8f-e8b2-4c4e-96fc-c86cdd7ed55f/oauth2/v2.0/authorize"),
-                TokenUrl = new Uri("https://login.microsoftonline.com/b3edbf8f-e8b2-4c4e-96fc-c86cdd7ed55f/oauth2/v2.0/token")
+                AuthorizationUrl = new Uri(builder.Configuration["AzureAd:Swagger:AuthorizationUrl"]),
+                TokenUrl = new Uri(builder.Configuration["AzureAd:Swagger:TokenUrl"])
             }
         },
         Type = SecuritySchemeType.OAuth2  
@@ -40,15 +37,14 @@ builder.Services.AddSwaggerGen(options => {
 });
 var app = builder.Build();
 
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(
         options => { 
-            options.OAuthAppName("miniapp");
-            options.OAuthClientId("109e12e2-4ca7-48d0-af05-c834c884322c");
-            options.OAuthScopes("109e12e2-4ca7-48d0-af05-c834c884322c/access_as_user");
+            options.OAuthAppName(builder.Configuration["AzureAd:AppName"]);
+            options.OAuthClientId(builder.Configuration["AzureAd:ClientId"]);
+            options.OAuthScopes(builder.Configuration["AzureAd:Scopes"]);
             options.OAuthUsePkce();
         }
     );
@@ -59,36 +55,8 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-var scopeRequiredByApi = app.Configuration["AzureAd:Scopes"] ?? "";
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-{
-    httpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
-
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.RequireAuthorization();
-
 app.MapGet("/", () => "Hello World!").RequireAuthorization( "Policy_Role_A" );
 
 app.MapFallback(() => Results.Redirect("/swagger"));
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
